b785fde4c619fa8916d14c81d97a4f3e2e652bf7
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-02-09 23:45:17 +0900

    MonthViewController 네비게이션 버튼 추가

--- TrainerCalendar/MonthViewController.swift
+++ TrainerCalendar/MonthViewController.swift
@@ -13,6 +13,26 @@ class MonthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        setupNavigationController()
+    }
+}
+
+extension MonthViewController {
+    func setupNavigationController() {
+        let searchButton = UIBarButtonItem(
+            image: UIImage(systemName: "magnifyingglass"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        
+        let plusButton = UIBarButtonItem(
+            image: UIImage(systemName: "plus"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        navigationItem.rightBarButtonItems = [plusButton, searchButton]
     }
 }
